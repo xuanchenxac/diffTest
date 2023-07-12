fe7d48df08d6e16e1b062f2f62a28b8174fe1f9b
“xuanchenxac” <xuanchenxac@gmail.com>
2023-07-12 13:19:26 +0800

    缩进修改

--- src/main/java/code1/code1.java
+++ src/main/java/code1/code1.java
@@ -6,7 +6,7 @@ public class code1 {
     public static void main(String[] args) {
 
 
-        
-        System.out.println("This is a test code.");
+
+                System.out.println("This is a test code.");
     }
 }
