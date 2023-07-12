907abcc4b1ace304f68d4f0b613bae6f58193cba
“xuanchenxac” <xuanchenxac@gmail.com>
2023-07-12 16:59:24 +0800

    GOOGLE new cat

--- src/main/java/code1/code1.java
+++ src/main/java/code1/code1.java
@@ -1,9 +1,12 @@
 package code1;
 
+import model.cat;
+
 //这是注释1的GOOGLE修改版本1
 public class code1 {
     //new comment
     public static void main(String[] args) {
         System.out.println("This is a test code.");
+        new cat();
     }
 }
