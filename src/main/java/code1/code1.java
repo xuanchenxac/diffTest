a8f45ff35b6dfa61a4b2a90d099a7c229a98e2bf
“xuanchenxac” <xuanchenxac@gmail.com>
2023-07-13 10:46:57 +0800

    GOOGLE:add innerClass

--- src/main/java/code1/code1.java
+++ src/main/java/code1/code1.java
@@ -1,7 +1,5 @@
 package code1;
-
 import model.cat;
-
 //这是注释1的GOOGLE修改版本1
 public class code1 {
     //new comment
--- src/main/java/code2/codes.java
+++ src/main/java/code2/codes.java
@@ -8,4 +8,8 @@ public class codes {
     public int add(int a, int b) {
         return a+b;
     }
+
+    private class InnerClass {
+        int b = 2;
+    }
 }
